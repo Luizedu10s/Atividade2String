73054b2ca7d5812440da2482694093cd5f7b1e19
LuizEdu10s <luiz-souza.ls@academico.ifpb.edu.br>
2025-05-16 20:54:06 -0300

    Resposta da questão 2

---
+++ src/q2.js
@@ -0,0 +1,8 @@
+// CRIAÇÃO DA FUNÇÃO
+function VerificarVazia(variavel){
+    if(variavel.length == 0){ // CONDIÇÃO PARA VERIFICAR SE A STRING ESTA VAZIA
+        return true; // SE A STRING ESTIVER VAZIA RETORNA TRUE
+    } else {
+        return false; // SE A STRING NÃO ESTIVER VAZIA RETORNA FALSE
+    }
+}
